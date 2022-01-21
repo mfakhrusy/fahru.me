47a330ab23bd3c8bcccb5c099d8953e197e1ca1d
Muhamad Fakhrusy <fakhrusy.m@gmail.com>
2022-01-21 10:29:57 -0500

    enhance content in education app

--- src/components/desktop/apps/AppEducation.tsx
+++ src/components/desktop/apps/AppEducation.tsx
@@ -1,6 +1,6 @@
 import { AppLayout } from "@/components/desktop/apps/AppLayout";
 import useDelayRenderOnTouchDevice from "@/lib/useDelayRenderOnTouchDevice";
-import { ListItem, Text, UnorderedList } from "@chakra-ui/react";
+import { Box, ListItem, Text, UnorderedList } from "@chakra-ui/react";
 import { MutableRefObject } from "react";
 
 type Props = {
@@ -22,17 +22,17 @@ export function AppEducation({ onClose, isOpen, dragConstraintRef }: Props) {
       {shouldRenderContent && (
         <UnorderedList listStyleType="none" ml="0">
           <ListItem mb="15px">
-            <Text fontWeight="600">Undergraduate University</Text>
-            <Text>Bandung Institute of Technology</Text>
+            <Text fontWeight="600">Undergraduate Degree</Text>
+            <Text>Bandung Institute of Technology, Indonesia</Text>
             <Text as="small" display="flex">
               2012 - 2016
             </Text>
             <Text as="small">3.76 / 4.00 GPA</Text>
             <Text>Major: Aerospace Engineering</Text>
+            <Box h={2} />
             <Text>
-              Undergrad thesis topic: Direct Numerical Simulation for
-              Computational Fluid Dynamic (forgot the exact title but it's good
-              enough)
+              Research Thesis: Direct Numerical Simulation for Computational
+              Fluid Dynamic (forgot the exact title but it's close enough)
             </Text>
           </ListItem>
           <ListItem mb="15px">
@@ -43,11 +43,10 @@ export function AppEducation({ onClose, isOpen, dragConstraintRef }: Props) {
               learnt from from whatever source I could find, but freecodecamp
               acted as my main study sources.
             </Text>
+            <Box h={2} />
             <Text>
               I didn't get certifications from the platform since I got my first
-              web development job while I was still learning there, and you
-              know, I'm too "lazy" to get the certification since I was too
-              focused on my newly acquired job and forgot to continue it.
+              web development job while I was still learning there.
             </Text>
           </ListItem>
         </UnorderedList>
