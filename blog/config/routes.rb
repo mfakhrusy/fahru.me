52adea35ed94ae802cb7e3c309614b48c15fb794
M Fahru <fakhrusy.m@gmail.com>
2023-02-07 00:36:23 -0700

    add fly.io config

--- blog/config/routes.rb
+++ blog/config/routes.rb
@@ -1,7 +1,6 @@
 Rails.application.routes.draw do
-  get 'articles/index'
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  root "articles#index"
+  get "/articles", to: "articles#index"
 
-  # Defines the root path route ("/")
-  # root "articles#index"
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
