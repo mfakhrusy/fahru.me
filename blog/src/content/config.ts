5a00ce5a3025033f320fe74662e7500716842527
M Fahru <fakhrusy.m@gmail.com>
2023-05-06 07:32:44 -0700

    yeet blog from main repo

--- blog/src/content/config.ts
+++
@@ -1,8 +0,0 @@
-import { defineCollection } from "astro:content";
-import { blogSchema } from "./_schemas";
-
-const blog = defineCollection({
-  schema: blogSchema,
-});
-
-export const collections = { blog };
