ba99e7cd935db0f1c52d36fcff93578a9176cf43
M Fahru <fakhrusy.m@gmail.com>
2023-05-14 15:35:28 -0700

    use `AppMarkdownLayout` on `AppAboutSite` component

--- client/src/components/desktop/apps/AppAboutSite.tsx
+++ client/src/components/desktop/apps/AppAboutSite.tsx
@@ -1,8 +1,93 @@
-import { ExternalLinkIcon } from "@chakra-ui/icons";
-import { Box, Link, ListItem, Text, UnorderedList } from "@chakra-ui/react";
 import type { MutableRefObject } from "react";
-import { AppLayout } from "@/components/desktop/apps/layout/AppLayout";
-import useDelayRenderOnTouchDevice from "@/lib/useDelayRenderOnTouchDevice";
+import { AppMarkdownLayout } from "@/components/desktop/apps/layout/AppMarkdownLayout";
+
+const md = `
+## **About site**
+
+This is Fahru personal site
+
+<br />
+
+## **Source Code**
+
+Available on github:
+
+[https://github.com/mfakhrusy/fahru.me](https://github.com/mfakhrusy/fahru.me) <ExternalLinkIcon />
+
+<br />
+
+## **Tech Stack & Libraries**
+
+- typescript
+- reactJS
+- nextJS
+- vercel
+- xtermjs
+- chakra-ui
+- framer-motion
+
+<br />
+
+## **Icons**
+
+[Moka Icons](https://snwh.org/moka) by [Sam Hewitt](https://samuelhewitt.com) 
+
+licensed under [CC-SA-4.0](https://creativecommons.org/licenses/by-sa/4.0/) <ExternalLinkIcon />
+
+<br />
+
+## **Background**
+
+[GitHub](https://github.com/PineAndApplePizza/open-wallpapers/) by PineAndApplePizza
+
+licensed under [GPL](https://www.gnu.org/licenses/gpl-3.0.html)
+
+original logo creator [u/Ishaan_P](https://www.reddit.com/user/Ishaan_P)
+
+<br />
+
+## **About site**
+
+This is Fahru personal site
+
+<br />
+
+## **Source Code**
+
+Available on github:
+
+[https://github.com/mfakhrusy/fahru.me](https://github.com/mfakhrusy/fahru.me) <ExternalLinkIcon />
+
+<br />
+
+## **Tech Stack & Libraries**
+
+- typescript
+- reactJS
+- nextJS
+- vercel
+- xtermjs
+- chakra-ui
+- framer-motion
+
+<br />
+
+## **Icons**
+
+[Moka Icons](https://snwh.org/moka) by [Sam Hewitt](https://samuelhewitt.com) 
+
+licensed under [CC-SA-4.0](https://creativecommons.org/licenses/by-sa/4.0/) <ExternalLinkIcon />
+
+<br />
+
+## **Background**
+
+[GitHub](https://github.com/PineAndApplePizza/open-wallpapers/) by PineAndApplePizza
+
+licensed under [GPL](https://www.gnu.org/licenses/gpl-3.0.html)
+
+original logo creator [u/Ishaan_P](https://www.reddit.com/user/Ishaan_P)  
+`;
 
 type Props = {
   onClose: () => void;
@@ -11,105 +96,13 @@ type Props = {
 };
 
 export function AppAboutSite({ onClose, isOpen, dragConstraintRef }: Props) {
-  const shouldRenderContent = useDelayRenderOnTouchDevice({ delayAmount: 150 });
-
   return (
-    <AppLayout
+    <AppMarkdownLayout
+      markdown={md}
       title="About Site"
       onClose={onClose}
       isOpen={isOpen}
       dragConstraintRef={dragConstraintRef}
-    >
-      {shouldRenderContent && (
-        <>
-          <Text fontWeight="bold">About site</Text>
-          <Text>This is Fahru personal site</Text>
-          <Box minH={4} />
-          <Text fontWeight="bold">Source Code</Text>
-          <Text>Available on github:</Text>
-          <Link
-            textDecor="underline"
-            target="_blank"
-            href="https://github.com/mfakhrusy/fahru.me"
-          >
-            https://github.com/mfakhrusy/fahru.me <ExternalLinkIcon />
-          </Link>
-          <Box minH={4} />
-          <Text fontWeight="bold">Tech Stack & Libraries</Text>
-          <UnorderedList>
-            <ListItem>typescript</ListItem>
-            <ListItem>reactJS</ListItem>
-            <ListItem>nextJS</ListItem>
-            <ListItem>vercel</ListItem>
-            <ListItem>xtermjs</ListItem>
-            <ListItem>chakra-ui</ListItem>
-            <ListItem>framer-motion</ListItem>
-          </UnorderedList>
-          <Box minH={4} />
-          <Text fontWeight="bold">Icons</Text>
-          <Text>
-            <Link
-              textDecor="underline"
-              target="_blank"
-              href="https://snwh.org/moka"
-            >
-              Moka Icons <ExternalLinkIcon />
-            </Link>
-            &nbsp;by&nbsp;
-            <Link
-              textDecor="underline"
-              target="_blank"
-              href="https://samuelhewitt.com"
-            >
-              Sam Hewitt <ExternalLinkIcon />
-            </Link>
-          </Text>
-          <Box minH={4} />
-          <Text>
-            licensed under&nbsp;
-            <Link
-              textDecor="underline"
-              target="_blank"
-              href="https://creativecommons.org/licenses/by-sa/4.0/"
-            >
-              CC-SA-4.0 <ExternalLinkIcon />
-            </Link>
-          </Text>
-          <Box minH={4} />
-          <Text fontWeight="bold">Background</Text>
-          <Text>
-            <Link
-              textDecor="underline"
-              target="_blank"
-              href="https://github.com/PineAndApplePizza/open-wallpapers/"
-            >
-              GitHub <ExternalLinkIcon />
-            </Link>
-            &nbsp;by&nbsp; PineAndApplePizza
-          </Text>
-          <Box minH={4} />
-          <Text>
-            licensed under&nbsp;
-            <Link
-              textDecor="underline"
-              target="_blank"
-              href="https://www.gnu.org/licenses/gpl-3.0.html"
-            >
-              GPL <ExternalLinkIcon />
-            </Link>
-          </Text>
-          <Text>
-            original logo creator&nbsp;
-            <Link
-              textDecor="underline"
-              target="_blank"
-              href="https://www.reddit.com/user/Ishaan_P/"
-            >
-              u/Ishaan_P <ExternalLinkIcon />
-            </Link>
-          </Text>
-        </>
-      )}
-    </AppLayout>
+    />
   );
 }
