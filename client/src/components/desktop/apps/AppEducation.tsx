09e6948e860186cd726eb968a54f6e18c150167f
M Fahru <fakhrusy.m@gmail.com>
2023-05-15 22:30:28 -0700

    migrate education to markdown layout

--- client/src/components/desktop/apps/AppEducation.tsx
+++ client/src/components/desktop/apps/AppEducation.tsx
@@ -1,56 +1,51 @@
-import { Box, ListItem, Text, UnorderedList } from "@chakra-ui/react";
 import type { MutableRefObject } from "react";
-import { AppLayout } from "@/components/desktop/apps/layout/AppLayout";
-import useDelayRenderOnTouchDevice from "@/lib/useDelayRenderOnTouchDevice";
+import { AppMarkdownLayout } from "@/components/desktop/apps/layout/AppMarkdownLayout";
+
+const md = `
+  
+## Undergraduate Degree
+
+Bandung Institute of Technology, Indonesia
+
+2012 - 2016
+
+3.76 / 4.00 GPA
+
+<br />
+
+Major: Aerospace Engineering
+
+<br />
+
+Research Thesis: Direct Numerical Simulation for Computational Fluid Dynamic (forgot the exact title but it's close enough)
+
+<br />
+
+## Freecodecamp
+
+2016 - 2018
+
+<br />
+
+My "formal" education for web development studies. I actually learnt from from whatever source I could find, but freecodecamp acted as my main study sources.
+
+<br />
+
+I didn't get certifications from the platform since I got my first web development job while I was still learning there.
+`;
 
 type Props = {
-  onClose: () => void;
   isOpen: boolean;
   dragConstraintRef?: MutableRefObject<HTMLDivElement>;
 };
 
-export function AppEducation({ onClose, isOpen, dragConstraintRef }: Props) {
-  const shouldRenderContent = useDelayRenderOnTouchDevice({ delayAmount: 150 });
-
+export function AppEducation({ isOpen, dragConstraintRef }: Props) {
   return (
-    <AppLayout
+    <AppMarkdownLayout
+      markdown={md}
       title="Education"
-      onClose={onClose}
       isOpen={isOpen}
       dragConstraintRef={dragConstraintRef}
-    >
-      {shouldRenderContent && (
-        <UnorderedList listStyleType="none" ml="0">
-          <ListItem mb="15px">
-            <Text fontWeight="600">Undergraduate Degree</Text>
-            <Text>Bandung Institute of Technology, Indonesia</Text>
-            <Text as="small" display="flex">
-              2012 - 2016
-            </Text>
-            <Text as="small">3.76 / 4.00 GPA</Text>
-            <Text>Major: Aerospace Engineering</Text>
-            <Box h={2} />
-            <Text>
-              Research Thesis: Direct Numerical Simulation for Computational
-              Fluid Dynamic (forgot the exact title but it&apos;s close enough)
-            </Text>
-          </ListItem>
-          <ListItem mb="15px">
-            <Text fontWeight="600">Freecodecamp</Text>
-            <small>2016 - 2018</small>
-            <Text>
-              My &quot;formal&quot; education for web development studies. I
-              actually learnt from from whatever source I could find, but
-              freecodecamp acted as my main study sources.
-            </Text>
-            <Box h={2} />
-            <Text>
-              I didn&apos;t get certifications from the platform since I got my
-              first web development job while I was still learning there.
-            </Text>
-          </ListItem>
-        </UnorderedList>
-      )}
-    </AppLayout>
+    />
   );
 }
