bf8f41eff557cb7e4564d970ab273e6b24e1b0cb
M Fahru <fakhrusy.m@gmail.com>
2023-05-19 21:17:52 -0700

    integrate wifi icon to taskbar menu state so all taskbar menu can sync with each other (no overlapping menu)

--- client/src/components/desktop/taskbar/TaskbarWifiIcon.tsx
+++ client/src/components/desktop/taskbar/TaskbarWifiIcon.tsx
@@ -2,34 +2,61 @@ import {
   Flex,
   IconButton,
   Popover,
+  PopoverArrow,
   PopoverBody,
   PopoverContent,
   PopoverTrigger,
   Text,
+  useDisclosure,
 } from "@chakra-ui/react";
+import { useEffect } from "react";
 import { BiWifi, BiWifiOff } from "react-icons/bi";
 import { useSelector } from "react-redux";
+import { useDispatch } from "react-redux";
 import type { RootState } from "@/store";
+import { type TaskbarState, setTaskbarMenu } from "@/store/taskbar";
 
 export function TaskbarWifiIcon() {
+  const dispatch = useDispatch();
+  const { isOpen, onClose, onOpen } = useDisclosure();
   const serverReachable = useSelector<RootState, boolean>(
     (state) => state.connection.serverReachable
   );
 
+  const taskbarMenu = useSelector<RootState, TaskbarState["taskbarMenu"]>(
+    (state) => state.taskbar.taskbarMenu
+  );
+
+  useEffect(() => {
+    // avoid double event firing, we need to introduce local state (isOpen)
+    // and then manually set taskbarMenu to "WifiIcon" when isOpen is true
+    // caveat: if closing the popover, taskbarMenu will still have "WifiIcon" as its value
+    if (isOpen === true && taskbarMenu !== "WifiIcon") {
+      dispatch(setTaskbarMenu("WifiIcon"));
+    }
+  }, [dispatch, isOpen, taskbarMenu]);
+
   return (
     <Flex ml="auto" alignItems="center">
-      <Popover placement="bottom">
+      <Popover
+        placement="bottom"
+        isOpen={isOpen}
+        onClose={onClose}
+        returnFocusOnClose={false}
+      >
         <PopoverTrigger>
           <IconButton
             aria-label="button"
             bgColor="transparent"
             minWidth="20px"
             _hover={{ bgColor: "transparent" }}
+            onClick={onOpen}
           >
             {serverReachable ? <BiWifi /> : <BiWifiOff />}
           </IconButton>
         </PopoverTrigger>
         <PopoverContent width="100%">
+          <PopoverArrow />
           <PopoverBody>
             <Text color="black" fontSize="12px">
               {serverReachable
