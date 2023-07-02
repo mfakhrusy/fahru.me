9bd827e7aa65c9d9ba8cbb2bb9215f667bc76fad
M Fahru <fakhrusy.m@gmail.com>
2023-07-02 07:45:39 -0700

    Fix terminal behaviour for android phones

--- client/src/components/shared/XTerm.tsx
+++ client/src/components/shared/XTerm.tsx
@@ -59,9 +59,65 @@ export default function XTerm(props: Props) {
       if (isAndroid) {
         terminal.write("guest@fakhrusy.com ~$ ");
         terminal.write("\x1b[?25l"); // remove cursor
+
+        let androidCommand = "";
+        let backspaceCount = 0;
         terminal.onData((data) => {
-          executeCommand({ command: data, terminal, router });
-          terminal.write("\r" + "guest@fakhrusy.com ~$ ");
+          if (encodeURIComponent(data) === "%7F") {
+            // %7F is a delete character
+            if (androidCommand.length > 0) {
+              backspaceCount += 1;
+              androidCommand = androidCommand.slice(0, -1 * backspaceCount);
+              androidCommand += decodeURIComponent(" ".repeat(backspaceCount));
+              terminal.write("\r" + "guest@fakhrusy.com ~$ ");
+              terminal.write(androidCommand);
+            }
+          } else if (encodeURIComponent(data) === "%0D") {
+            // if it's carriage return, do nothing since it has been handled in onKey
+          } else {
+            if (backspaceCount > 0) {
+              androidCommand = androidCommand.slice(0, -1 * backspaceCount);
+              backspaceCount = 0;
+            }
+
+            androidCommand += data;
+            terminal.write("\r" + "guest@fakhrusy.com ~$ ");
+            terminal.write(androidCommand);
+          }
+        });
+
+        terminal.onKey((e) => {
+          const ev = e.domEvent;
+
+          if (ev.key === "Enter") {
+            if (
+              encodeURIComponent(androidCommand) === "%0D" ||
+              androidCommand.trim().length === 0 // all whitespace
+            ) {
+              androidCommand = "";
+            }
+
+            executeCommand({ command: androidCommand, terminal, router });
+
+            if (androidCommand === "reboot" || androidCommand === "shutdown") {
+              setActiveDesktopApp("DesktopMainView");
+
+              terminal.write("\r\n" + "guest@fakhrusy.com ~$ ");
+            } else if (androidCommand === "") {
+              terminal.write("\r" + "guest@fakhrusy.com ~$ ");
+            } else {
+              terminal.write("\r\n" + "guest@fakhrusy.com ~$ ");
+            }
+
+            androidCommand = "";
+          } else if (ev.key === "Backspace") {
+            // do nothing just in case
+            // since the backspace for android is handled in onData
+          } else if (!checkIfArrowKey(e.key)) {
+            androidCommand += e.key;
+
+            terminal.write(e.key);
+          }
         });
       } else {
         terminal.write("\r\n");
